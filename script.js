548df654da37b3de3b92850bea67b78cff6e47fe
David Bielik <davidbielik@gmail.com>
2021-12-20 11:51:30 -0500

    combined into one function

--- script.js
+++ script.js
@@ -1,53 +1,55 @@
-
-
-function computerPlay() {
-    const computerChoiceArray = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random()*computerChoiceArray.length);
-    return computerSelection = computerChoiceArray[random];   
-}
-
-function playerPrompt() {
-let playerPromptSelection=prompt('Type rock, paper, or scissors to play against the computer.');
-playerSelection = playerPromptSelection.toLowerCase();
-}
-
-    function winnerLoser(computerSelection, playerSelection) {
-    if (computerSelection==='rock'&&playerSelection==='rock') {
-        return ('Computer chose rock. It\'s a tie...')
-    }
-    else if (computerSelection==='rock'&&playerSelection==='paper') {
-        return ('Computer chose rock. You won!')
-    }
-    else if (computerSelection==='rock'&&playerSelection==='scissors') {
-        return ('Computer chose rock. You lose. :(')
-    }
-    else if (computerSelection==='paper'&&playerSelection==='rock') {
-        return ('Computer chose paper. You lose. :(')
-    }
-    else if (computerSelection==='paper'&&playerSelection==='paper') {
-        return ('Computer chose paper. It\'s a tie...')
-    }
-    else if (computerSelection==='paper'&&playerSelection==='scissors') {
-        return ('Computer chose paper. You win!')
-    }
-    else if (computerSelection==='scissors'&&playerSelection==='rock') {
-        return ('Computer chose scissors. You win!')
-    }
-    else if (computerSelection==='scissors'&&playerSelection==='paper') {
-        return ('Computer chose scissors. You lose. :(')
-    }
-    else if (computerSelection==='scissors'&&playerSelection==='scissors') {
-        return ('Computer chose scissors. It\'s a tie...')
-    }    else return ('Please type rock, paper, or scissors to start.')}
-
 function oneRound() {
+    function computerPlay() {
+        const computerChoiceArray = ['rock', 'paper', 'scissors'];
+        const random = Math.floor(Math.random()*computerChoiceArray.length);
+        return computerSelection = computerChoiceArray[random];   
+    }
+    function playerPrompt() {
+        let playerPromptSelection=prompt('Type rock, paper, or scissors to play against the computer. Best of 5 rounds wins.');
+        playerSelection = playerPromptSelection.toLowerCase();
+    }
+        
     computerPlay();
     playerPrompt();
+
+    function winnerLoser(computerSelection, playerSelection) {
+        if (computerSelection==='rock'&&playerSelection==='rock') {
+            return ('Computer chose rock. It\'s a tie...')
+        }
+        else if (computerSelection==='rock'&&playerSelection==='paper') {
+            return ('Computer chose rock. You won!')
+        }
+        else if (computerSelection==='rock'&&playerSelection==='scissors') {
+            return ('Computer chose rock. You lose. :(')
+        }
+        else if (computerSelection==='paper'&&playerSelection==='rock') {
+            return ('Computer chose paper. You lose. :(')
+        }
+        else if (computerSelection==='paper'&&playerSelection==='paper') {
+            return ('Computer chose paper. It\'s a tie...')
+        }
+        else if (computerSelection==='paper'&&playerSelection==='scissors') {
+            return ('Computer chose paper. You win!')
+        }
+        else if (computerSelection==='scissors'&&playerSelection==='rock') {
+            return ('Computer chose scissors. You win!')
+        }
+        else if (computerSelection==='scissors'&&playerSelection==='paper') {
+            return ('Computer chose scissors. You lose. :(')
+        }
+        else if (computerSelection==='scissors'&&playerSelection==='scissors') {
+            return ('Computer chose scissors. It\'s a tie...')
+        }    
+        else return ('Invalid choice.')
+    }
+    winnerLoser();
     console.log('You chose '+playerSelection+'.')
     console.log(winnerLoser(computerSelection, playerSelection));
 }
 
 function game() {
+    let playerPoints=0;
+    let computerPoints=0;
     oneRound();
     oneRound();
     oneRound();
@@ -56,4 +58,5 @@ function game() {
 }
 
 game();
+
 console.log('Game over. Refresh the page to start again.')
