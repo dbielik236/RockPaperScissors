c436115bb503e0af406197d4f39254f3ab6ce3b9
David Bielik <davidbielik@gmail.com>
2021-12-31 13:15:26 -0500

    wording changes

--- script.js
+++ script.js
@@ -1,167 +1,5 @@
-
-/*
-function computerPlay() {
-    const computerChoiceArray = ['rock', 'paper', 'scissors'];
-    const random = Math.floor(Math.random()*computerChoiceArray.length);
-    return computerSelection = computerChoiceArray[random];   
-}
-computerPlay();
-
-function playerChoice() {
-    const rockBtn = document.querySelector('.rockButton');
-    rockBtn.addEventListener('click', () => {
-        const playerSelection='rock';
-        playRound(computerSelection, playerSelection);
-
-        //player DOM
-        const container = document.querySelector('#playerSelection');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'Player choice: '+playerSelection;
-        container.appendChild(content);
-        
-        //computer DOM
-        const cmptSelection = document.querySelector('#computerSelection');
-        const cmptText = document.createElement('div');
-        cmptText.classList.add('cmptContent');
-        cmptText.textContent = 'Computer choice: '+computerSelection;
-        cmptSelection.appendChild(cmptText);
-    });
-
-
-    const paperBtn = document.querySelector('.paperButton');
-    paperBtn.addEventListener('click', () => {
-        const playerSelection='paper';
-        playRound(computerSelection, playerSelection);
-
-        //player DOM
-        const container = document.querySelector('#playerSelection');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'Player choice: '+playerSelection;
-        container.appendChild(content);
-        
-        //computer DOM
-        const cmptSelection = document.querySelector('#computerSelection');
-        const cmptText = document.createElement('div');
-        cmptText.classList.add('cmptContent');
-        cmptText.textContent = 'Computer choice: '+computerSelection;
-        cmptSelection.appendChild(cmptText);
-    });
-
-    const scissorsBtn = document.querySelector('.scissorsButton');
-    scissorsBtn.addEventListener('click', () => {
-        const playerSelection='scissors';
-        playRound(computerSelection, playerSelection);
-
-        //player DOM
-        const container = document.querySelector('#playerSelection');
-        const content = document.createElement('div');
-        content.classList.add('content');
-        content.textContent = 'Player choice: '+playerSelection;
-        container.appendChild(content);
-        
-        //computer DOM
-        const cmptSelection = document.querySelector('#computerSelection');
-        const cmptText = document.createElement('div');
-        cmptText.classList.add('cmptContent');
-        cmptText.textContent = 'Computer choice: '+computerSelection;
-        cmptSelection.appendChild(cmptText);
-    });
-}
-playerChoice();
-
-function playRound(computerSelection, playerSelection) {    
-    function determineWinner() {
-        if (computerSelection==='rock'&&playerSelection=='rock') {
-            return (outcome='Computer chose rock. It\'s a tie...')
-        }
-        else if (computerSelection==='rock'&&playerSelection==='paper') {
-            return (outcome='Computer chose rock. You win the round!')
-        }
-        else if (computerSelection==='rock'&&playerSelection==='scissors') {
-            return (outcome='Computer chose rock. You lose the round. :(')
-        }
-        else if (computerSelection==='paper'&&playerSelection=='rock') {
-            return (outcome='Computer chose paper. You lose the round. :(')
-        }
-        else if (computerSelection==='paper'&&playerSelection==='paper') {
-            return (outcome='Computer chose paper. It\'s a tie...')
-        }
-        else if (computerSelection==='paper'&&playerSelection==='scissors') {
-            return (outcome='Computer chose paper. You win the round!')
-        }
-        else if (computerSelection==='scissors'&&playerSelection=='rock') {
-            return (outcome='Computer chose scissors. You win the round!')
-        }
-        else if (computerSelection==='scissors'&&playerSelection==='paper') {
-            return (outcome='Computer chose scissors. You lose the round. :(')
-        }
-        else if (computerSelection==='scissors'&&playerSelection==='scissors') {
-            return (outcome='Computer chose scissors. It\'s a tie...')
-        }    
-        else return ('Invalid choice.')
-    }
-    determineWinner();
-
-    //outcome DOM
-    const roundResult = document.querySelector('#roundResults');
-    const results = document.createElement('div');
-    results.classList.add('winnerRound');
-    results.textContent = outcome;
-    roundResult.appendChild(results);
-
-    function allocatingPoints() {
-        if (outcome==='Computer chose rock. You win the round!' || 
-            outcome==='Computer chose paper. You win the round!'||
-            outcome==='Computer chose scissors. You win the round!') {
-            return winner='Player Wins'
-        }
-        else if (outcome==='Computer chose rock. You lose the round. :('||
-            outcome==='Computer chose paper. You lose the round. :('||
-            outcome==='Computer chose scissors. You lose the round. :(') {
-            return winner='Computer Wins'
-        }
-        else return winner='No points'
-    }
-    allocatingPoints();
-}    
-function game() {
-    for (let i = 1; i < 6; i++) {   
-        playRound();
-    let playerPoints=0;
-    let computerPoints=0;
-    
-    function assignPoints() {
-        if (winner=='Computer Wins') {
-            return computerPoints+=1
-        }
-        else if (winner=='Player Wins') {
-        return playerPoints+=1
-        }
-        else return;
-    }
-
-    assignPoints();
-    console.log(computerPoints, playerPoints)   
-    }       
-
-    function determineWinner() {
-        if (playerPoints>computerPoints) {
-            return gameResult='You won the game. Great job!'
-        }
-        else if (computerPoints>playerPoints) {
-            return gameResult='You lost the game. But don\'t let it bother you!'
-        }
-        else return gameResult='The game was a draw. So... you both won?!'
-    }
-    determineWinner();
-
-}
-    
-*/
-
 function playRound() {
+    
     let playerPoints = 0;
     let computerPoints = 0;
 
@@ -175,6 +13,7 @@ function playRound() {
 
     initiateRound();
     function initiateRound() {
+        
         const rockBtn = document.querySelector('.rockButton');
         rockBtn.addEventListener('click', () => {
             const playerSelection='rock';
@@ -182,6 +21,7 @@ function playRound() {
             if (playerPoints<5&&computerPoints<5) {
             computerPlay();
             
+             
             //player DOM
             const container = document.querySelector('#playerSelection');
             const content = document.createElement('div');
@@ -260,11 +100,11 @@ function playRound() {
                 
                 assignPoints();
                   
-                //outcome DOM
+                //score DOM
                 const roundScore = document.querySelector('#runningScore');
                 const score = document.createElement('div');
                 score.classList.add('roundScore');
-                score.textContent = 'Computer points: '+computerPoints+'  Player points: ' +playerPoints;
+                score.textContent = 'Computer: '+computerPoints+'  Player: ' +playerPoints;
                 roundScore.appendChild(score);
                 }
                 else return
@@ -370,11 +210,11 @@ function playRound() {
                 }
                 
                 assignPoints();
-                //outcome DOM
+                //score DOM
                 const roundScore = document.querySelector('#runningScore');
                 const score = document.createElement('div');
                 score.classList.add('roundScore');
-                score.textContent = 'Computer points: '+computerPoints+'  Player points: ' +playerPoints;
+                score.textContent = 'Computer: '+computerPoints+'  Player: ' +playerPoints;
                 roundScore.appendChild(score);
                 }
                 else return
@@ -411,6 +251,7 @@ function playRound() {
             content.classList.add('content');
             content.textContent = playerSelection;
             container.appendChild(content);
+            
             
             //computer DOM
             const cmptSelection = document.querySelector('#computerSelection');
@@ -482,13 +323,14 @@ function playRound() {
                 
                 assignPoints();
                 
-                //outcome DOM
+                //score DOM
                 const roundScore = document.querySelector('#runningScore');
                 const score = document.createElement('div');
                 score.classList.add('roundScore');
-                score.textContent = 'Computer points: '+computerPoints+'  Player points: ' +playerPoints;
+                score.textContent = 'Computer: '+computerPoints+'  Player: ' +playerPoints;
                 roundScore.appendChild(score);
                 }
+                
                 else return
 
                 if (playerPoints == 5) {
@@ -507,19 +349,16 @@ function playRound() {
                 youLose.textContent = 'Game over: You lost';
                 computerWins.appendChild(youLose);
                 }
-                
+                 
         });
-    
+        
     }
 
 }
+playRound();
 
-function game () {
-    playRound();
     
-}
 
-game ();
 
 
 
